997f3729d4c510eb3a19cce80909a39c621846ab
jwaskow <thewasko@gmail.com>
2017-05-17 19:50:37 -0400

    Add collision detection
    Ball now bounces within the walls of the canvas.  Also doubled ball's
    framerate of drawing.

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ let ctx = canvas.getContext("2d");
 let x = canvas.width/2;
 let y = canvas.height-30;
 
-let dx = 2;
-let dy = -2;
+let dx = 1;
+let dy = -1;
+
+let ballRadius = 10;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "#9957db";
   ctx.fill();
   ctx.closePath();
@@ -26,9 +28,15 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
   x += dx;
   y += dy;
 }
-setInterval(draw, 10);
+setInterval(draw, 5);
 
 });
